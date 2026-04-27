e4a05c155874a388f0e0e9d4c87d0e5c21257427
said lopez <dev@cocxy.dev>
2026-04-27 05:44:42 +0000

    Update cocxy to 0.1.89

--- Casks/cocxy.rb
+++ Casks/cocxy.rb
@@ -1,6 +1,6 @@
 cask "cocxy" do
-  version "0.1.88"
-  sha256 "ba1b3d678002130d7f3b798625bb642c00b25996928346d6b13700f5d5303c53"
+  version "0.1.89"
+  sha256 "a9c83c148d0b8a414125115c7a99762afb9a8b21a4c0f6509a0d03c00cbdd3dd"
 
   url "https://github.com/salp2403/cocxy-terminal/releases/download/v#{version}/CocxyTerminal-#{version}.dmg"
   name "Cocxy Terminal"
